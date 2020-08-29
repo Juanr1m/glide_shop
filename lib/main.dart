320462ddb81eb38fddf394bf4f9aaa4706feff6c
R1m <rimkarimov20@gmail.com>
2020-08-29 20:11:35 +0500

    fix homepage

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
             headline3: DetailPriceTextStyle,
             headline4: DetailDescBarTextStyle,
           )),
-      home: HomeScreen(),
+      home: HomePage(),
     );
   }
 }
